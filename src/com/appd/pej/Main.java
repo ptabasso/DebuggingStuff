1ae9958759e5eaaa053d9e66afdcbd7ec6173b52
ptabasso <pejman.tabassomi@appdynamics.com>
2017-11-09 14:46:21 +0100

    Initial

--- src/com/appd/pej/Main.java
+++ src/com/appd/pej/Main.java
@@ -24,6 +24,32 @@ public class Main {
     public static void callingFromTest() throws InterruptedException {
         System.out.println("I'm in submethod");
         Thread.sleep(Long.parseLong("2000"));
+        callTwo();
     }
+
+    public static void callTwo() throws InterruptedException {
+        System.out.println("I'm in two");
+        Thread.sleep(200);
+        callThree();
+    }
+
+    public static void callThree() throws InterruptedException {
+        System.out.println("I'm in three");
+        Thread.sleep(200);
+        callFour();
+    }
+
+    public static void callFour() throws InterruptedException {
+        System.out.println("I'm in four");
+        Thread.sleep(200);
+        callFive();
+    }
+
+    public static void callFive() throws InterruptedException {
+        System.out.println("I'm in five");
+        Thread.sleep(200);
+    }
+
+
 
 }
